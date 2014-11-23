f5afd11316e7b9462d19e2e01488d06d2b6fa11a
mangopop <simon.norton@gmail.com>
2014-11-23 23:16:20 +0000

    routing working with ids and routeparams. Didn't realise the hash was so important in the URL. check loops more carefully in the future as well!

--- main.js
+++ main.js
@@ -1,17 +1,28 @@
 var myApp = angular.module('myApp', ['ngRoute']);
 
-myApp.config(function($routeProvider){
+myApp.config(['$routeProvider',function($routeProvider){ //don't have to add first argument
     $routeProvider.when("/",
         {
-            templateUrl:"showVideos.html",
+            templateUrl:"partials/allVideos.html",
             controller:'vidsCtrl'
         }
+    ).when("/singleVideo/:id",
+        {
+            templateUrl:"partials/singleVideo.html",
+            controller:'singleVidCtrl'
+        }
     )
-});
+    .otherwise(
+        {
+            redirectTo: '/',
+            templateUrl:"allVideos.html"
 
-myApp.factory('videos', [function () {
+        }
+    )
+}]).factory('videos', [function () {
     var videos = [
         {
+            id:'1',
             title: 'Intersteller',
             year: '2014',
             director: 'Christopher Nolan',
@@ -19,6 +30,7 @@ myApp.factory('videos', [function () {
             rating:'1'
         },
         {
+            id:'2',
             title: 'Terminator',
             year: '1989',
             director: 'James Cameron',
@@ -26,6 +38,7 @@ myApp.factory('videos', [function () {
             rating:'2'
         },
         {
+            id:'3',
             title: 'Kung fu Hustle',
             year: '2005',
             director: 'Chinese man',
@@ -33,6 +46,7 @@ myApp.factory('videos', [function () {
             rating:'3'
         },
         {
+            id:'4',
             title: 'Star Wars',
             year: '1979',
             director: 'Irvine Something',
@@ -40,6 +54,7 @@ myApp.factory('videos', [function () {
             rating:'4'
         },
         {
+            id: '5',
             title: 'Iron Man',
             year: '2010',
             director: 'Irvine Something',
@@ -47,13 +62,14 @@ myApp.factory('videos', [function () {
             rating:'5'
         },
         {
+            id:'6',
             title: 'Fury',
             year: '2014',
             director: 'Irvine Something',
             url: 'images/fury.jpg',
             rating:'4'
         }
-        ];
+    ];
 
     return videos;
 }]);
@@ -61,7 +77,33 @@ myApp.factory('videos', [function () {
 myApp.controller('vidsCtrl', ['$scope', 'videos', function ($scope, videos) {
 
     $scope.videos = videos;
-
     var videoCount = $scope.videos.length;
 
+}]);
+
+myApp.controller('singleVidCtrl', ['$scope', 'videos', '$routeParams', function($scope, videos, $routeParams){
+    $scope.params = {
+        id: $routeParams.id
+    };
+
+    $scope.videos = videos;
+    var videoCount = $scope.videos.length;
+
+    console.log($scope.videos[0].id, $scope.params.id);
+
+    var getVideo = function(videoObj,paramObj){
+        console.log(videoObj.length);
+        for(var i = 0, ilen=videoObj.length; i < ilen; i++){
+
+            if(videoObj[i].id == paramObj.id){
+                console.log(videoObj[i].id);
+                return videoObj[i];
+
+            }
+        }
+
+    };
+
+    $scope.requestedVideo = getVideo($scope.videos,$scope.params);
+    console.log($scope.requestedVideo);
 }]);
